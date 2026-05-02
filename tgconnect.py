15c22ae6156c062af72a82aaa7a7ce9e6d337d56
dcuzic <dcuzic1@gmail.com>
2026-05-02 13:35:12 +0100

    added test_connection

--- tgconnect.py
+++ tgconnect.py
@@ -12,4 +12,11 @@ client = TelegramClient("session", api_id, api_hash)
 client.start()
 print("Client started")
 
-# wip test tg connection
+# test tg connection
+async def test_connection():
+    me = await client.get_me()
+    print(f"Username: {me.username}, ID: {me.id}")
+
+with client:
+    client.loop.run_until_complete(test_connection())
+
