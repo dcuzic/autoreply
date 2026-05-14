67f4270a02bd2de3e1c0af4a264ddd8beb352793
dcuzic <dcuzic1@gmail.com>
2026-05-14 22:28:26 +0100

    positioned start button, main frame, and added replies_sent frame

--- gui.py
+++ gui.py
@@ -2,21 +2,30 @@ import customtkinter as ctk
 from pathlib import Path
 
 app = ctk.CTk()
-app.geometry("500x500")
+app.geometry("400x540")
+app.resizable(False, False)
 app.title("AutoReply")
+app.configure(fg_color=("#30302e"))
 
+frame = ctk.CTkFrame(app, width=350, height=230, corner_radius=10, fg_color="#262624")
+frame.grid(row=0, column=0, padx=25, pady=35)
 
-frame = ctk.CTkFrame(app, width=400, height=150, fg_color="grey")
-frame.grid(row=0, column=0, padx=15, pady=15)
-
-change_busy_intervals_btn = ctk.CTkButton(app,
-                                                    width=100, 
-                                                    height=25, 
-                                                    corner_radius=5, 
-                                                    text="Change busy intervals", 
+start_btn = ctk.CTkButton(app,
+                                                    width=350, 
+                                                    height=35, 
+                                                    fg_color="transparent",
+                                                    border_color="grey",
+                                                    border_width=1,
+                                                    corner_radius=10, 
+                                                    text="Start", 
                                                     font=ctk.CTkFont(family="SF Pro Display", size=14)
                                                     )
-change_busy_intervals_btn.grid(row=0, column=0, padx=20, pady=20)
+start_btn.grid(row=1, column=0, padx=20, pady=0)
+
+replies_summary_frame = ctk.CTkFrame(app, 
+                                     width=170,
+                                     height=70,
+                                     fg_color="#262624")
+replies_summary_frame.grid(padx=0, pady=20, )
 
 app.mainloop()
-
