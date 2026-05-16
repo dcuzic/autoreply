78aaca8a459c88d86c004eb71281ca72abd07f69
dcuzic <dcuzic1@gmail.com>
2026-05-16 16:49:18 +0100

    added primary status text, not positioned

--- gui.py
+++ gui.py
@@ -10,8 +10,15 @@ app.configure(fg_color=("#30302e"))
 app.columnconfigure(0, weight=1)
 app.columnconfigure(1, weight=1)
 
-frame = ctk.CTkFrame(app, width=350, height=230, corner_radius=10, fg_color="#262624")
-frame.grid(row=0, column=0, columnspan=2, padx=25, pady=35)
+# --- status frame
+
+# frame
+status_frame = ctk.CTkFrame(app, width=350, height=230, corner_radius=10, fg_color="#262624")
+status_frame.grid(row=0, column=0, columnspan=2, padx=25, pady=35)
+
+# status text
+status_text_stopped_primary = ctk.CTkLabel(status_frame, text="Stopped", font=ctk.CTkFont(family="SF Pro Display", size=28))
+status_text_stopped_primary.grid(row=0, column=0, padx=175, pady=115)
 
 start_btn = ctk.CTkButton(app,
                                                     width=350, 
@@ -38,8 +45,7 @@ report_time_frame = ctk.CTkFrame(app,
                                  )
 report_time_frame.grid(row=2, column=1, padx=(5, 25), pady=0, sticky="w")
 
-# --- bottom buttons (home, whitelist, report, settings)
-
+# --- navigation buttons (home, whitelist, report, settings)
 
 # frame
 nav_frame = ctk.CTkFrame(app, fg_color="#30302e", corner_radius=0)
@@ -64,10 +70,5 @@ home_btn.grid(row=0, column=2, sticky="ew", ipady=15)
 
 home_btn = ctk.CTkButton(nav_frame, text="⚙\nSettings", fg_color="transparent", corner_radius=5, border_color="grey", border_width=1)
 home_btn.grid(row=0, column=3, sticky="ew", ipady=15)
-
-
-
-
-
 
 app.mainloop()
